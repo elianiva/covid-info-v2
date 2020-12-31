be762ae0c0117785c7ceac667b80c659ff1906b5
elianiva <dicha.arkana03@gmail.com>
2020-12-31 11:00:42 +0700

    feat: added more grid variants

--- utils/twind.setup.ts
+++ utils/twind.setup.ts
@@ -2,12 +2,19 @@ import { setup } from "twind"
 
 setup({
   theme: {
-    extend: {
-    },
+    extend: {},
   },
   plugins: {
-    'main-grid': {
-      'grid-template-columns': 'repeat(4, 1fr) 1.25fr'
-    }
-  }
+    "main-grid": {
+      "grid-template-columns": "repeat(4, 1fr) 1.25fr",
+    },
+    "map-grid": {
+      "grid-template-columns": "2fr 1fr",
+      "grid-template-rows": "2.5rem 1fr 1fr 1fr",
+      "gap": "1rem",
+    },
+    "nunito": {
+      "font-family": "'Nunito', sans-serif",
+    },
+  },
 })
