f6e1c76ca8565964864fc67066e0c083191c0497
elianiva <dicha.arkana03@gmail.com>
2020-12-31 15:22:11 +0700

    feat: use SSR instead of SSG

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import { GetStaticProps } from "next"
+import Head from "next/head"
+import { GetServerSideProps } from "next"
 import { useEffect, useState } from "react"
 import { tw } from "twind"
 import { MapData, fetchData } from "../utils"
@@ -7,11 +8,7 @@ import MapBox from "../components/MapBox"
 import ChartBox from "../components/ChartBox"
 import * as countryData from "../data/countries.json"
 
-export default function Home({
-  apiData,
-}: {
-  apiData: Country[]
-}): JSX.Element {
+export default function Home({ apiData }: { apiData: Country[] }): JSX.Element {
   const [mapData, setMapData] = useState<CountryGeoJSON | null>(null)
 
   useEffect(() => {
@@ -20,17 +17,22 @@ export default function Home({
   }, [])
 
   return (
-    <div className={tw`grid main-grid grid-rows-4 gap-4 min-h-screen p-4`}>
-      <ChartBox className="col-start-1 col-end-2" />
-      <ChartBox className="col-start-2 col-end-3" />
-      <ChartBox className="col-start-3 col-end-4" />
-      <ChartBox className="col-start-4 col-end-5" />
-      <MapBox mapData={mapData} apiData={apiData} />
-    </div>
+    <>
+      <Head>
+        <title>Covid Info</title>
+      </Head>
+      <div className={tw`grid main-grid grid-rows-4 gap-4 min-h-screen p-4`}>
+        <ChartBox className="col-start-1 col-end-2" />
+        <ChartBox className="col-start-2 col-end-3" />
+        <ChartBox className="col-start-3 col-end-4" />
+        <ChartBox className="col-start-4 col-end-5" />
+        <MapBox mapData={mapData} apiData={apiData} />
+      </div>
+    </>
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const response: Country[] = await fetchData<Country[]>("countries")
 
   return {
