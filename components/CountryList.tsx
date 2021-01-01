fc88857166d0a7cec2e29f092c30daedae5f8989
elianiva <dicha.arkana03@gmail.com>
2021-01-01 19:20:28 +0700

    feat: scrollbar styling

--- components/CountryList.tsx
+++ components/CountryList.tsx
@@ -1,4 +1,5 @@
-import { tw } from "twind"
+import { tw, CSSRules } from "twind"
+import { css } from "twind/css"
 import { Country } from "../types"
 import CountryItem from "./CountryItem"
 
@@ -7,21 +8,33 @@ interface CountryListProps {
 }
 
 export default function CountryList({ countries }: CountryListProps) {
+  const scrollbarStyle: CSSRules = {
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: "#d4d4d4",
+      borderRadius: "0.5rem",
+      cursor: "pointer"
+    },
+    "&:hover::-webkit-scrollbar-thumb": {
+      backgroundColor: "#b0b0b0"
+    },
+    "&::-webkit-scrollbar": {
+      "background-color": "#ffffff",
+      width: "0.5rem",
+    },
+  }
+
   return (
     <div className={tw`contents`}>
-      <div
-        className={tw`flex items-center col(start-2 end-3) row(start-1 end-2)`}
+      <span
+        className={tw`flex items-center col(start-2 end-3) row(start-1 end-2) nunito font-bold text-lg`}
       >
-        <span className={tw`flex-1 nunito font-bold text-lg`}>
-          Top Countries
-        </span>
-        <div className={tw`flex items-center gap-2`}>
-          <button className={tw`px-4 py-2 rounded-md shadow-md`}>&lt;</button>
-          <button className={tw`px-4 py-2 rounded-md shadow-md`}>&gt;</button>
-        </div>
-      </div>
+        Top Countries
+      </span>
       <div
-        className={tw`grid(& row-10) gap-2 row(start-2 end-5) col(start-2 end-3) h-full overflow-y-auto`}
+        className={tw`
+          grid(& row-10) gap-2 row(start-2 end-5) col(start-2 end-3) h-full overflow-y-auto pr-1
+          ${css(scrollbarStyle)}
+        `}
       >
         {countries?.map((country, idx) => (
           <CountryItem
