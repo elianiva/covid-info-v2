abe07f4d3643d73c0fe9dea6090ebdcc7ec676d7
elianiva <dicha.arkana03@gmail.com>
2021-01-01 20:29:53 +0700

    refactor: rename grid plugins

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app"
-import "../styles/globals.css"
+import "../assets/styles/globals.css"
 import "../utils/twind.setup"
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
--- utils/twind.setup.ts
+++ utils/twind.setup.ts
@@ -9,13 +9,13 @@ setup({
     },
   },
   plugins: {
-    "main-grid": {
+    "grid-main": {
       "grid-template-columns": "repeat(4, 1fr) 1.25fr",
     },
     "gauge-grid": {
       "grid-template-columns": "1fr 2fr",
     },
-    "map-grid": {
+    "grid-map": {
       "grid-template-columns": "2fr 1fr",
       "grid-template-rows": "2.5rem 1fr 1fr 1fr",
       "gap": "1rem",
